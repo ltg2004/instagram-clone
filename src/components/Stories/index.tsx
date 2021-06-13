709d0ca4da8c0c0435b8883628e571006fd5d7b2
ltg <ltg@bbangyanetworks.com>
2021-06-14 01:18:46 +0900

    useEffect 추가

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import Story from '../Story';
 import axios from 'axios';
@@ -8,22 +8,20 @@ type StoriesData = {
   name: string;
 };
 // let storyData: StoriesData[] = [];
-function reducer(state: StoriesData[], action: StoriesData[]) {
-  return action;
-}
 const Stories = () => {
-  const [items, dispatch] = useReducer(reducer, []);
-  axios.get('http://localhost:3000/stories').then(res => {
-    const {code, data} = res.data;
-    if (code === '2000') {
-      dispatch(data);
-      // setItems(data);
-      // return;
-      // data.forEach((el) => {
-      //   // setItems([el]);
-      // });
-    }
-  });
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    let mounted = true;
+    axios.get('http://localhost:3000/stories').then(res => {
+      const {code, data} = res.data;
+      if (code === '2000') {
+        if (mounted) {
+          setItems(data);
+        }
+      }
+    });
+    return () => mounted = false;
+  }, []);
   return (
     <FlatList
       data={items}
